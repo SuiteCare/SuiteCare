03ced0afff1490bc97327670661c58f526f36aaa
Jae Hee Park <jaeheepark@Jaeui-MacBookPro.local>
2023-12-06 01:20:00 +0900

    ADD: layout, home, header, footer, about, contact 등 마이그레이션 작업중

---
+++ src/components/Home/Contact/index.jsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import { Fade, Slide } from 'react-reveal';
+
+const Contact = ({ data }) => {
+  if (!data) return null;
+
+  const [formData, setFormData] = useState({
+    contactName: '',
+    contactEmail: '',
+    contactSubject: '',
+    contactMessage: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <section id='contact'>
+      <Fade bottom duration={1000}>
+        <div className='row section-head'>
+          <div className='two columns header-col'>
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
+
+          <div className='ten columns'>
+            <p className='lead'>{data.contactmessage}</p>
+          </div>
+        </div>
+      </Fade>
+
+      <div className='row'>
+        <Slide left duration={1000}>
+          <div className='eight columns'>
+            <form action='' method='post' id='contactForm' name='contactForm'>
+              <fieldset>
+                <div>
+                  <label htmlFor='contactName'>
+                    Name <span className='required'>*</span>
+                  </label>
+                  <input
+                    type='text'
+                    defaultValue=''
+                    size='35'
+                    id='contactName'
+                    name='contactName'
+                    value={formData.contactName}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor='contactEmail'>
+                    Email <span className='required'>*</span>
+                  </label>
+                  <input
+                    type='text'
+                    defaultValue=''
+                    size='35'
+                    id='contactEmail'
+                    name='contactEmail'
+                    value={formData.contactEmail}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor='contactSubject'>Subject</label>
+                  <input
+                    type='text'
+                    defaultValue=''
+                    size='35'
+                    id='contactSubject'
+                    name='contactSubject'
+                    value={formData.contactSubject}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor='contactMessage'>
+                    Message <span className='required'>*</span>
+                  </label>
+                  <textarea
+                    cols='50'
+                    rows='15'
+                    id='contactMessage'
+                    name='contactMessage'
+                    value={formData.contactMessage}
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+
+                <div>
+                  <button className='submit'>Submit</button>
+                  <span id='image-loader'>
+                    <img alt='' src='images/loader.gif' />
+                  </span>
+                </div>
+              </fieldset>
+            </form>
+
+            <div id='message-warning'> Error boy</div>
+            <div id='message-success'>
+              <i className='fa fa-check'></i>Your message was sent, thank you!
+              <br />
+            </div>
+          </div>
+        </Slide>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,42 @@
+import React, { useState, useEffect, createContext } from 'react';
+import ReactGA from 'react-ga';
+import axios from 'axios';
+
+import styles from './Home.module.css';
+import Header from '@/components/Home/Header';
+import Footer from '@/components/Home/Footer';
+import Contact from '@/components/Home/Contact';
+import About from '@/components/Home/About';
+
 const Home = () => {
+  const [indexData, setIndexData] = useState(Object);
+
+  useEffect(() => {
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+    getIndexData();
+  }, []);
+
+  const getIndexData = async () => {
+    try {
+      const response = await axios.get('./indexData.json');
+      const data = response.data;
+      setIndexData(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.Home}>
+      <StateContext.Provider vaule={indexData}>
+        <Header data={indexData.main} />
+        <About data={indexData.about} />
+        <Contact data={indexData.main} />
+        <Footer data={indexData.main} />
+      </StateContext.Provider>
     </div>
   );
 };
 
+export const StateContext = createContext();
 export default Home;
