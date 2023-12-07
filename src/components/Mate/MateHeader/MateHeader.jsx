58a991f8d7c6caef0acb810fe1284a09c76e4e8a
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-12-07 16:57:39 +0900

    [feat] : Mate 폴더 마이그레이션
    - Mate / MateHeader 마이그레이션
    - 메인카드에서 background 변수 삭제

---
+++ src/components/Mate/MateHeader/MateHeader.jsx
@@ -0,0 +1,11 @@
+import HeaderCore from '@/components/Common/Header/HeaderCore';
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <HeaderCore type={'mate'} />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Mate/MateHeader/MateMenuList.jsx
@@ -0,0 +1,16 @@
+const MenuList = [
+  {
+    title: '일감 검색하기',
+    url: 'search',
+  },
+  {
+    title: '내 활동 관리',
+    url: 'manage',
+  },
+  {
+    title: '마이페이지',
+    url: 'mypage',
+  },
+];
+
+export default MenuList;
