25a08dfd4ea6b0104cbdb11a06c193090c441379
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-12-07 16:17:11 +0900

    [feat] : Family, Mate 공통 컴포넌트 마이그레이션
    - Header / HeaderCore, Dropdown
    - Login / LoginForm
    - Main / MainCard
    - SignUp / SignUpForm
    - 로고, 배경 그림 용량최적화 버전

---
+++ src/components/Common/Header/HeaderCore.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styles from './HeaderCore.module.css';
+import Logo from '../../assets/img/logo/logo-full-highres.png';
+import Dropdown from './Dropdown';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const HeaderCore = ({ type }) => {
+  const [familyMenuOpen, setFamilyMenuOpen] = useState(false);
+  const [mateMenuOpen, setMateMenuOpen] = useState(false);
+
+  const toggleMenu = ($type) => {
+    if ($type === 'family') {
+      setFamilyMenuOpen(!familyMenuOpen);
+      setMateMenuOpen(false);
+    } else if ($type === 'mate') {
+      setMateMenuOpen(!mateMenuOpen);
+      setFamilyMenuOpen(false);
+    }
+  };
+
+  return (
+    <div className={styles.HeaderCore}>
+      <Link id='logo' href={`/${type}/main`}>
+        <Image src={Logo} alt='Logo' />
+      </Link>
+      <div className='nav_wrapper'>
+        <ul className='menu_wrapper'>
+          <li>
+            <button onClick={() => toggleMenu('family')} className={`${type === 'family' ? 'active' : ''}`}>
+              간병인 찾기
+            </button>
+            <Dropdown type='family' isOpen={familyMenuOpen} />
+          </li>
+          <li>
+            <button onClick={() => toggleMenu('mate')} className={`${type === 'mate' ? 'active' : ''}`}>
+              간병 일감 찾기
+            </button>
+            <Dropdown type='mate' isOpen={mateMenuOpen} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCore;
---
+++ src/components/Common/Login/LoginForm.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import styles from './LoginForm.module.css';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+const LoginForm = ({ type }) => {
+  // react hook에서 state 사용
+  const [ID, setID] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const onIDHandler = (event) => {
+    setID(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    // 버튼만 누르면 리로드 되는것을 막아줌
+    event.preventDefault();
+    console.log('ID', ID);
+    console.log('Password', Password);
+
+    axios
+      .post('/api/v1/login', {
+        suite_family_id: 'Kim',
+        password: 'Kim',
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('로그인 결과: ');
+          console.log('ID : ' + response.data.suite_family_id);
+          console.log('PW: ' + response.data.password);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const navigator = useRouter();
+
+  const goToSignUp = () => {
+    navigator.push(`/${type}/signup`);
+  };
+
+  return (
+    <div className={styles.LoginForm}>
+      <div class='login_wrapper'>
+        <h1> 로그인 </h1>
+        <br />
+        <form onSubmit={onSubmitHandler}>
+          <div class='login_input_wrapper'>
+            <input type='text' placeholder=' 아이디' value={ID} onChange={onIDHandler} />
+          </div>
+          <div class='login_input_wrapper'>
+            <input type='password' placeholder=' 비밀번호' value={Password} onChange={onPasswordHandler} />
+          </div>
+          <br />
+          <button className='button button_1' type='submit'>
+            로그인
+          </button>
+          <button className='button button_2' onClick={goToSignUp}>
+            회원가입
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
