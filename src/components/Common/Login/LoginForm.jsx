c2f2a57dcd17352f873fe669a9d1a876a65f7dbf
Lee Jae Sun <ze_s96@naver.com>
2023-12-12 12:02:51 +0900

    [fix] import 오류 수정
    - axios import 추가

--- src/components/Common/Login/LoginForm.jsx
+++ src/components/Common/Login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './LoginForm.module.css';
+import axios from 'axios';
 
 const LoginForm = ({ type }) => {
   const navigator = useRouter();
