05bac2b14907a3d16d6a7a0c1d1e3f22683a04ab
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-12-26 12:49:10 +0900

    [fix] 사이드바 링크로 변경

--- src/components/Family/FamilyReservationList/FamilyReservationSidebar.jsx
+++ src/components/Family/FamilyReservationList/FamilyReservationSidebar.jsx
@@ -1,12 +1,22 @@
+import Link from 'next/link';
+
 import styles from '@/components/Common/Sidebar.module.css';
 
 const FamilyReservationSidebar = () => {
   return (
     <div className={styles.Sidebar}>
-      <div className={styles.menu}>내 일정 정보</div>
-      <div className={styles.menu}>간병 신청 내역</div>
-      <div className={styles.menu}>결제 대기</div>
-      <div className={styles.menu}>간병 완료 목록</div>
+      <div className={styles.menu}>
+        <Link href='/'>내 일정 정보</Link>
+      </div>
+      <div className={styles.menu}>
+        <Link href='/'>간병 신청 내역</Link>
+      </div>
+      <div className={styles.menu}>
+        <Link href='/'>결제 대기</Link>
+      </div>
+      <div className={styles.menu}>
+        <Link href='/'>간병 완료 목록</Link>
+      </div>
     </div>
   );
 };
