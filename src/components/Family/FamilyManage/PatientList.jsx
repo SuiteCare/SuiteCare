51b6ed059afe119ef51246bb68ec541cd1e37dab
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-12-28 17:37:26 +0900

    [feat] family manage 부분 3depth 최종파일 경로 변경 / PatientList 빈 값일 때 메시지 표시 추가

--- src/components/Family/FamilyManage/PatientList.jsx
+++ src/components/Family/FamilyManage/PatientList.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
 import styles from './FamilyManageTable.module.css';
-
 import { calAge } from '@/utils/calculators';
+import { useRouter } from 'next/router';
 
 const PatientList = ({ data }) => {
+  const navigator = useRouter();
+
   return (
     <div className={styles.FamilyManageTable}>
       <table>
         <thead>
           <tr>
-            <th>인덱스</th>
+            <th>#</th>
             <th>성함</th>
             <th>성별</th>
             <th>생년월일/나이</th>
@@ -21,22 +22,34 @@ const PatientList = ({ data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((e, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{e.name}</td>
-              <td>{e.gender === 'F' ? '여자' : '남자'}</td>
-              <td>
-                {e.birthday} (만 {calAge(e.birthday)}세)
-              </td>
-              <td>{e.height} cm</td>
-              <td>{e.weight} kg</td>
-              <td>{e.diagnosis_name}</td>
-              <td>
-                <button>상세정보 보기</button>
+          {data.length === 0 ? (
+            <tr>
+              <td colSpan={8}>
+                <br />
+                등록한 환자가 없습니다.
+                <br /> <br />
+                <button onClick={() => navigator.push('/family/addPatient')}>환자 등록하기</button>
+                <br /> <br />
               </td>
             </tr>
-          ))}
+          ) : (
+            data.map((e, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{e.name}</td>
+                <td>{e.gender === 'F' ? '여자' : '남자'}</td>
+                <td>
+                  {e.birthday} (만 {calAge(e.birthday)}세)
+                </td>
+                <td>{e.height} cm</td>
+                <td>{e.weight} kg</td>
+                <td>{e.diagnosis_name}</td>
+                <td>
+                  <button>상세정보 보기</button>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
