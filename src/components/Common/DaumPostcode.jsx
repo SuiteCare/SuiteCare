49bf2365f2e9a8a21b28cd608912bb5c65ee6e86
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-01-04 17:56:33 +0900

    [feat] Daum 주소검색 API 추가 및 Request 명세 수정

---
+++ src/components/Common/DaumPostcode.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+
+const DaumPostcode = ({ address, setAddress }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const closeDaumPostcode = () => {
+    setIsPopupVisible(false);
+  };
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const execDaumPostcode = () => {
+    new window.daum.Postcode({
+      oncomplete: (data) => {
+        setAddress({
+          postcode: data.zonecode,
+          roadAddress: data.roadAddress,
+          jibunAddress: data.jibunAddress,
+          detailAddress: '',
+        });
+
+        setIsPopupVisible(false);
+      },
+    }).open();
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setAddress({ ...address, [id]: value });
+  };
+
+  const renderPopup = () => {
+    if (!isPopupVisible) return null;
+
+    return (
+      <div
+        id='layer'
+        style={{
+          display: 'block',
+          position: 'fixed',
+          overflow: 'hidden',
+          zIndex: 1,
+          WebkitOverflowScrolling: 'touch',
+        }}
+      >
+        <img
+          src='//t1.daumcdn.net/postcode/resource/images/close.png'
+          id='btnClosePopup'
+          style={{
+            cursor: 'pointer',
+            position: 'absolute',
+            right: '-3px',
+            top: '-3px',
+            zIndex: 1,
+          }}
+          onClick={closeDaumPostcode}
+          alt='닫기 버튼'
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className='DaumPostcode'>
+      <input
+        type='text'
+        id='postcode'
+        placeholder='우편번호'
+        value={address.postcode}
+        onChange={handleInputChange}
+        maxLength={5}
+        pattern='[0-9]{5}'
+      />
+      <button type='button' onClick={execDaumPostcode}>
+        우편번호 찾기
+      </button>
+      <input
+        type='text'
+        id='roadAddress'
+        placeholder='도로명주소'
+        value={address.roadAddress}
+        onChange={handleInputChange}
+      />
+      <input
+        type='text'
+        id='jibunAddress'
+        placeholder='지번주소'
+        value={address.jibunAddress}
+        onChange={handleInputChange}
+      />
+      <input
+        type='text'
+        id='detailAddress'
+        placeholder='상세주소'
+        value={address.detailAddress}
+        onChange={handleInputChange}
+      />
+
+      {renderPopup()}
+    </div>
+  );
+};
+
+export default DaumPostcode;
