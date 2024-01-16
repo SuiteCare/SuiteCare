0c45ffaf12542ba1bff94bba0730bd57ca80ba97
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-01-16 13:00:47 +0900

    [chore] 검색결과 텍스트 표현 수정

--- src/components/Family/FamilyMateSearch/SearchResult.jsx
+++ src/components/Family/FamilyMateSearch/SearchResult.jsx
@@ -71,8 +71,10 @@ const SearchResult = ({ data, type }) => {
     <div className={`${styles.SearchResult} Form_wide`}>
       <h3>
         {type === 'search'
-          ? `${data.length ? data.length : 0}명의 메이트님을 찾았습니다. 지금 간병을 신청해 보세요!`
-          : `스위트케어가 추천하는 메이트`}
+          ? data.length
+            ? `${data.length}명의 메이트님을 찾았습니다. 지금 간병을 신청해 보세요!`
+            : '나에게 꼭 맞는 메이트님을 찾아보세요!'
+          : '스위트케어가 추천하는 메이트'}
       </h3>
       {data && data.length > 0 ? (
         data.map((e) => <SearchResultCard data={e} key={e.id} showDetail={() => handleShowModal(e)} />)
