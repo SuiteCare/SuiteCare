9b363131ddcae21585669574129f26855d1ca72c
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-02-21 17:39:14 +0900

    [feat] 마이페이지 patch용으로 request body 수정

--- src/components/Common/MyPage/MyPageForm.jsx
+++ src/components/Common/MyPage/MyPageForm.jsx
@@ -6,60 +6,106 @@ import { useMutation, useQuery } from 'react-query';
 import useLoginInfo from '@/hooks/useLoginInfo';
 import axiosInstance from '@/services/axiosInstance';
 import useModal from '@/hooks/useModal';
+import useAlert from '@/hooks/useAlert';
 
 import ChangePwModal from './ChangePwModal';
 import styles from './MyPageForm.module.css';
 
+import { genderToKo } from '@/utils/calculators';
+
 const MyPageForm = () => {
   const navigator = useRouter();
+  const { openAlert, alertComponent } = useAlert();
+  const { isModalVisible, openModal, closeModal } = useModal();
+  const { id } = useLoginInfo();
 
-  const { token, id } = useLoginInfo();
-  const [tel, setTel] = useState('');
+  const [formData, setFormData] = useState({
+    email: '',
+    tel: '',
+  });
+  const [changedData, setChangedData] = useState();
 
   const { data, isError, isLoading } = useQuery(
-    ['mypage', token],
+    ['mypage', id],
     async () => {
       const response = await axiosInstance.get('/api/v1/mypage', { params: { id } });
       return response.data;
     },
     {
-      enabled: Boolean(token),
+      enabled: Boolean(id),
     },
   );
 
   useEffect(() => {
     if (data) {
-      setTel(data.tel);
+      setFormData(data);
     }
   }, [data]);
 
-  const { isModalVisible, openModal, closeModal } = useModal();
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+    setChangedData((prevData) => ({
+      ...prevData,
+      [id]: id === 'tel' ? value.replaceAll('-', '') : value,
+    }));
+  };
 
-  const handlePhoneCertification = (event) => {
-    event.preventDefault();
+  // 휴대폰 번호 인증
+  const [isPhoneCertificated, setIsPhoneCertificated] = useState(false);
+  const handlePhoneCertification = (e) => {
+    e.preventDefault();
 
-    if (/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/.test(tel)) {
-      alert(`인증 api 연동 필요\n${tel}`);
-    } else {
-      alert('휴대폰 번호를 올바르게 입력하십시오.');
+    if (!/^01[0-9]{1}-?[0-9]{3,4}-?[0-9]{4}$/.test(formData.tel)) {
+      return openAlert('휴대폰 번호를 올바르게 입력하십시오.');
+    }
+
+    if (formData.tel) {
+      if (isPhoneCertificated) {
+        return openAlert('휴대폰 인증이 완료된 상태입니다.');
+      }
+      setIsPhoneCertificated(true);
+      return openAlert(`휴대폰 인증 api 연동 전 임시 인증 완료`);
+    }
+  };
+
+  // 이메일 인증
+  const [isEmailCertificated, setIsEmailCertificated] = useState(false);
+  const handleEmailCertification = (e) => {
+    e.preventDefault();
+
+    if (
+      !/(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/.test(
+        formData.email,
+      )
+    ) {
+      return openAlert('이메일 주소를 올바르게 입력하십시오.');
+    }
+
+    if (formData.email) {
+      if (isEmailCertificated) {
+        return openAlert('이메일 인증이 완료된 상태입니다.');
+      }
+      setIsEmailCertificated(true);
+      return openAlert(`이메일 인증 api 연동 전 임시 인증 완료`);
     }
   };
 
   const submitModification = useMutation(
     async () => {
-      const response = await axiosInstance.patch('/api/v1/member', {
-        tel: tel.replaceAll('-', ''),
-      });
-      console.log(tel, data.tel);
+      const response = await axiosInstance.patch('/api/v1/member', changedData);
       return response.data;
     },
     {
       onSuccess: ($data) => {
         if ($data) {
-          alert('내 정보 수정에 성공하였습니다.');
+          openAlert('내 정보 수정에 성공하였습니다.');
           navigator.reload();
         } else {
-          alert('내 정보 수정에 실패하였습니다.');
+          openAlert('내 정보 수정에 실패하였습니다.');
         }
       },
     },
@@ -68,15 +114,21 @@ const MyPageForm = () => {
   const handleModifyClick = async (e) => {
     e.preventDefault();
 
-    if (tel !== data.tel) {
-      submitModification.mutate();
+    if (formData.tel === data.tel && formData.email === data.email) {
+      openAlert('변경된 정보가 없습니다.');
+    } else if (!isPhoneCertificated) {
+      openAlert('휴대폰 인증이 필요합니다.');
+    } else if (!isEmailCertificated) {
+      openAlert('이메일 인증이 필요합니다.');
     } else {
-      alert('변경된 정보가 없습니다.');
+      submitModification.mutate();
     }
   };
 
   return (
     <div className={`${styles.MyPageForm} Form_narrow`}>
+      {alertComponent}
+
       <hr />
       <form name='MyPageForm' method='post'>
         <div className='input_wrapper'>
@@ -98,6 +150,26 @@ const MyPageForm = () => {
         </div>
 
         <div className='input_wrapper'>
+          <label>생년월일</label>
+          <input type='text' name='birthday' id='birthday' readOnly value={data?.birthday} />
+        </div>
+
+        <div className='input_wrapper'>
+          <label>성별</label>
+          <input type='text' name='gender' id='gender' readOnly value={`${genderToKo(data?.gender)}성`} />
+        </div>
+
+        <div className='input_wrapper'>
+          <label>이메일</label>
+          <div className='input_with_button'>
+            <input type='text' name='email' id='email' value={formData.email} onChange={(e) => handleInputChange(e)} />
+            <button type='button' onClick={handleEmailCertification}>
+              재인증
+            </button>
+          </div>
+        </div>
+
+        <div className='input_wrapper'>
           <label>휴대전화</label>
           <div className='input_with_button'>
             <div className={styles.input_phone}>
@@ -105,9 +177,9 @@ const MyPageForm = () => {
                 type='text'
                 placeholder='010-0000-0000'
                 id='tel'
-                value={tel}
+                value={formData.tel}
                 maxLength={13}
-                onChange={(e) => setTel(e.target.value)}
+                onChange={(e) => handleInputChange(e)}
               />
             </div>
             <button type='button' onClick={handlePhoneCertification}>
