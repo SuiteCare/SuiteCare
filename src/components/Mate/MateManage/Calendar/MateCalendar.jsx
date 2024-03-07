b341a151b9672c7e504ae604d8e64921600bd123
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-03-07 16:32:36 +0900

    [test] 캘린더 이벤트 불러오기 로직 수정 중

--- src/components/Mate/MateManage/Calendar/MateCalendar.jsx
+++ src/components/Mate/MateManage/Calendar/MateCalendar.jsx
@@ -28,10 +28,14 @@ const MateCalendar = () => {
 
   const { token } = useLoginInfo();
 
-  const { data, isError, isLoading } = useQuery(
+  const {
+    data: dbData,
+    isError,
+    isLoading,
+  } = useQuery(
     ['reservationList', token],
     async () => {
-      const response = await axiosInstance.get('/api/v1/mate/reservation');
+      const response = await axiosInstance.get('/api/v1/reservation/mate');
       return response.data;
     },
     {
@@ -40,32 +44,91 @@ const MateCalendar = () => {
   );
 
   useEffect(() => {
+    const data = [
+      {
+        id: 0,
+        recruitment_id: 0,
+        family_id: 'string',
+        family_name: 'string',
+        confirm_at: '2024-03-07',
+        start_date: '2024-03-07',
+        end_date: '2024-03-17',
+        start_time: {
+          hour: 10,
+          minute: 0,
+          second: 0,
+          nano: 0,
+        },
+        end_time: {
+          hour: 22,
+          minute: 0,
+          second: 0,
+          nano: 0,
+        },
+        weekdays: [0, 1, 2, 3, 4, 5, 6],
+      },
+      {
+        id: 1,
+        recruitment_id: 1,
+        family_id: 'string',
+        family_name: 'string',
+        confirm_at: '2024-03-07',
+        start_date: '2024-03-10',
+        end_date: '2024-03-31',
+        start_time: {
+          hour: 10,
+          minute: 0,
+          second: 0,
+          nano: 0,
+        },
+        end_time: {
+          hour: 22,
+          minute: 0,
+          second: 0,
+          nano: 0,
+        },
+        weekdays: [1, 2, 3, 4, 5],
+      },
+    ];
+
+    console.log('Mate Calendar용 데이터를 가져오는가?', data);
+
     const getEventList = () => {
-      let currentStartDate = moment(`${data.start_date} ${data.start_time}`);
-      let currentEndDate = moment(`${data.start_date} ${data.end_time}`);
-      const endDate = moment(`${data.end_date} ${data.end_time}`);
-      const weekdays = data?.weekdays;
+      data.forEach((eventItem) => {
+        let currentStartDate = moment(
+          `${eventItem.start_date} ${eventItem.start_time.hour}:${eventItem.start_time.minute}`,
+        );
+        let currentEndDate = moment(`${eventItem.start_date} ${eventItem.end_time.hour}:${eventItem.end_time.minute}`);
+        const endDate = moment(`${eventItem.end_date} ${eventItem.end_time.hour}:${eventItem.end_time.minute}`);
+        const weekdays = eventItem?.weekdays;
 
-      const events = [];
+        console.log('잘나오고있나ㅠ', currentStartDate, currentEndDate, endDate, weekdays);
 
-      // 주어진 범위 내의 출근 요일에 해당하는 날짜를 별개의 이벤트로 추가
-      while (currentEndDate.isSameOrBefore(endDate, 'day')) {
-        const dayOfWeek = moment(currentEndDate).format('ddd');
-        if (weekdays.includes(dayOfWeek)) {
-          const event = {
-            title: `${data.patient_name} 님 (${data.diagnosis_name})`,
-            family: `보호자 ${data.family_name} 님`,
-            start: new Date(currentStartDate),
-            end: new Date(currentEndDate),
-            color: stringToColor(data.patient_name + data.diagnosis_name + data.family_name),
-          };
-          events.push(event);
+        const events = [];
+
+        // 주어진 범위 내의 출근 요일에 해당하는 날짜를 별개의 이벤트로 추가
+        while (currentEndDate.isSameOrBefore(endDate, 'day')) {
+          const dayOfWeek = moment(currentEndDate).format('ddd');
+          if (weekdays.includes(dayOfWeek)) {
+            const event = {
+              title: `${eventItem.patient_name} 님 (${eventItem.diagnosis_name || '진단명 없음'})`,
+              family: `보호자 ${eventItem.family_name} 님`,
+              start: new Date(currentStartDate),
+              end: new Date(currentEndDate),
+              color: stringToColor(
+                (eventItem.patient_name || '환자명도 없음') +
+                  (eventItem.diagnosis_name || '진단명 없음') +
+                  eventItem.family_name,
+              ),
+            };
+            events.push(event);
+          }
+          currentStartDate = currentStartDate.add(1, 'day');
+          currentEndDate = currentEndDate.add(1, 'day');
         }
-        currentStartDate = currentStartDate.add(1, 'day');
-        currentEndDate = currentEndDate.add(1, 'day');
-      }
 
-      setEventList(events);
+        setEventList(events);
+      });
     };
 
     if (data && data.length > 0) {
