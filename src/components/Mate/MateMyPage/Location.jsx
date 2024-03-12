a865b2fbeef773c27d4123db7d0ac7c25c89f7b6
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-03-12 14:25:49 +0900

    [feat] Location 최대 5개까지 선택 가능하게 변경

--- src/components/Mate/MateMyPage/Location.jsx
+++ src/components/Mate/MateMyPage/Location.jsx
@@ -1,17 +1,34 @@
 import React, { useEffect } from 'react';
 
+import useAlert from '@/hooks/useAlert';
+
 import FormLocationList from '@/components/Common/SearchInfo/FormLocationList';
 
 const Location = ({ styles, formData, setFormData, setChangedData }) => {
+  const { openAlert, alertComponent } = useAlert();
+
   const handleCheckboxChange = (checked, value) => {
-    console.log(formData.locationList);
-    setFormData((prevFormData) => {
-      const updatedData = { ...prevFormData };
+    if (checked && formData.locationList.length >= 5) {
+      const slicedList = formData.locationList.slice(0, 5);
+      setFormData((prevData) => {
+        const updatedData = { ...prevData };
+        updatedData.locationList = [...slicedList];
+        return updatedData;
+      });
+      setChangedData((prevData) => {
+        const updatedData = { ...prevData };
+        updatedData.locationList = [...slicedList];
+        return updatedData;
+      });
+      return openAlert('활동 지역은 최대 5개까지 선택할 수 있습니다.');
+    }
+    setFormData((prevData) => {
+      const updatedData = { ...prevData };
 
       if (checked) {
-        updatedData.locationList = [...prevFormData.locationList, value];
+        updatedData.locationList = [...prevData.locationList, value];
       } else {
-        updatedData.locationList = prevFormData.locationList.filter((it) => it !== value);
+        updatedData.locationList = prevData.locationList.filter((it) => it !== value);
       }
 
       return updatedData;
@@ -38,6 +55,7 @@ const Location = ({ styles, formData, setFormData, setChangedData }) => {
 
   return (
     <>
+      {openAlert && alertComponent}
       <h3>활동 지역</h3>
       <div className={styles.checkbox_list_wrapper}>
         <FormLocationList onChange={(e) => handleCheckboxChange(e.currentTarget.checked, e.currentTarget.value)} />
