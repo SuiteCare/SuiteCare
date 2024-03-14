da8adf8ce82e8ad362beddf4a8dc2b2b306e97a0
Jae Hee Park <jaeheepark@Jaeui-MacBookPro.local>
2024-03-14 12:07:19 +0900

    [styled]: 내 활동 관리 메뉴 스타일링

--- src/components/Family/FamilyManage/FamilyManageSidebar.jsx
+++ src/components/Family/FamilyManage/FamilyManageSidebar.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
+import arrow from '@/assets/arrow.svg';
 import styles from '@/components/Common/Sidebar.module.css';
 
-const MenuItem = ({ url, label, isActive }) => (
-  <div className={`${styles.menu} ${isActive ? styles.active : ''}`}>
+const MenuItem = ({ url, label }) => (
+  <div className={styles.menuItem}>
     <Link href={url}>{label}</Link>
   </div>
 );
 
-const FamilyManageSidebar = ({ activeMenu }) => {
+const FamilyManageSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const defaultUrl = '/family/manage';
 
   const menuItems = [
@@ -19,12 +23,22 @@ const FamilyManageSidebar = ({ activeMenu }) => {
     { key: 'history', label: '전체 내역', url: `${defaultUrl}/history` },
   ];
 
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.Sidebar}>
-      <h3>내 활동 관리</h3>
-      {menuItems.map((item) => (
-        <MenuItem key={item.key} url={item.url} label={item.label} isActive={activeMenu === item.key} />
-      ))}
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : styles.close}`}>
+      <div onClick={toggleSidebar} className={styles.toggleButton}>
+        <input type='checkbox' />
+        <Image src={arrow} alt='arrowImage' width={30} height={30} />
+      </div>
+      <h3>내 활동관리</h3>
+      <div className={styles.menu}>
+        {menuItems.map((item) => (
+          <MenuItem key={item.key} url={item.url} label={item.label} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Mate/MateManage/MateManageSidebar.jsx
+++ src/components/Mate/MateManage/MateManageSidebar.jsx
@@ -1,22 +1,42 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
+import arrow from '@/assets/arrow.svg';
 import styles from '@/components/Common/Sidebar.module.css';
 
-const MateManageSidebar = ({ activeMenu }) => {
+const MenuItem = ({ url, label }) => (
+  <div className={styles.menuItem}>
+    <Link href={url}>{label}</Link>
+  </div>
+);
+
+const MateManageSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const defaultUrl = '/mate/manage';
+
+  const menuItems = [
+    { key: 'calendar', label: '내 일정 정보', url: `${defaultUrl}/calendar` },
+    { key: 'offer', label: '간병 요청 목록', url: `${defaultUrl}/offer` },
+    { key: 'apply', label: '공고 지원 목록', url: `${defaultUrl}/apply` },
+    { key: 'history', label: '전체 내역', url: `${defaultUrl}/history` },
+  ];
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.Sidebar}>
-      <h3>내 활동 관리</h3>
-      <div className={`${styles.menu} ${activeMenu === 'calendar' ? styles.active : ''}`}>
-        <Link href='/mate/manage/calendar'>내 일정 정보</Link>
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : styles.close}`}>
+      <div onClick={toggleSidebar} className={styles.toggleButton}>
+        <input type='checkbox' />
+        <Image src={arrow} alt='arrowImage' />
       </div>
-      <div className={`${styles.menu} ${activeMenu === 'offer' ? styles.active : ''}`}>
-        <Link href='/mate/manage/offer'>간병 요청 목록</Link>
-      </div>
-      <div className={`${styles.menu} ${activeMenu === 'apply' ? styles.active : ''}`}>
-        <Link href='/mate/manage/apply'>공고 지원 목록</Link>
-      </div>
-      <div className={`${styles.menu} ${activeMenu === 'history' ? styles.active : ''}`}>
-        <Link href='/mate/manage/history'>전체 내역</Link>
+      <h3>내 활동관리</h3>
+      <div className={styles.menu}>
+        {menuItems.map((item) => (
+          <MenuItem key={item.key} url={item.url} label={item.label} />
+        ))}
       </div>
     </div>
   );
