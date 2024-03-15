ac0b9b75539a6132912da49e3d4948699695e28c
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-03-15 23:42:20 +0900

    [feat] 결제 필요 리스트 표시
    - Reservation List 불러오기 코드 수정
    - PaymentCard에서 detail을 불러오도록 수정

--- src/components/Family/FamilyManage/PendingPayment/PendingPaymentCard.js
+++ src/components/Family/FamilyManage/PendingPayment/PendingPaymentCard.js
@@ -1,64 +1,99 @@
 import Image from 'next/image';
+import { useQuery } from 'react-query';
 
-import usePatientList from '@/services/apis/usePatientList';
+import axiosInstance from '@/services/axiosInstance';
 
 import styles from './PaymentCard.module.css';
 import defaultProfile from '@/assets/default_profile.jpg';
+import LocalLoading from '@/components/Common/Modal/LocalLoading';
 
-import { countWeekdays } from '@/utils/calculators';
+import { calTimeDiff, countWeekdays, normalizeWeekDays, weekdayDic } from '@/utils/calculators';
 
-const PaymentCard = ({ id, data, handleReservationDetailButton, handlePaymentButton }) => {
-  const { patientList } = usePatientList(id);
-  const selectPatient = ($id) => patientList?.find((e) => e.id === $id);
+const PaymentCard = ({ data, handleReservationDetailButton, handlePaymentButton }) => {
+  const recruitmentId = data.recruitment_id;
 
-  console.log(id);
-  console.log(data);
-  console.log(selectPatient(data.patient_id));
+  const {
+    data: detailData,
+    isError: isDetailDataError,
+    isLoading: isDetailDataLoading,
+  } = useQuery(
+    ['detailData', recruitmentId],
+    async () => {
+      const [recruitmentDetailResponse, patientDetailResponse] = await Promise.all([
+        axiosInstance.get(`/api/v1/recruitment/${recruitmentId}/detail`),
+        axiosInstance.get(`/api/v1/recruitment/${recruitmentId}/patient`),
+      ]);
+
+      const recruitmentDetail = recruitmentDetailResponse.data;
+      const patientDetail = patientDetailResponse.data;
+
+      return { ...recruitmentDetail, ...patientDetail };
+    },
+    {
+      enabled: Boolean(recruitmentId),
+    },
+  );
 
   return (
     <div className={styles.card}>
+      {isDetailDataLoading && <LocalLoading />}
       {data?.profile_picture_filename || <Image src={defaultProfile} alt='profile_picture' />}
       <div className={styles.contents}>
         <div className={styles.userName}>
-          <label>{data.mate_id}의 이름..</label>메이트
+          <span>간병 메이트 </span>
+          <label>
+            {data.mate_name} ({data.mate_resume_id})
+          </label>
           <hr />
         </div>
         <div className={styles.userInfo_wrapper}>
           <div className={styles.userInfo}>
+            <label>간병 위치</label>
+            <span>{detailData?.location}</span>{' '}
+            <span>
+              {detailData?.road_address} {detailData?.address_detail}
+            </span>
+          </div>
+          <div className={styles.userInfo}>
             <label>담당 환자</label>
             <span>
-              {selectPatient(data.patient_id)?.name} ({selectPatient(data.patient_id)?.diagnosis_name})
+              {detailData?.patient_name} ({detailData?.patient_diagnosis_name})
             </span>
           </div>
           <div className={styles.userInfo}>
             <label>간병 기간</label>
             <span>
-              {data.start_date} ~ {data.end_date} (총 {countWeekdays(data.start_date, data.end_date, '0,1,2')}일)
+              {data.start_date} ~ {data.end_date} (총 {countWeekdays(data.start_date, data.end_date, data.weekdays)}
+              일)
             </span>
           </div>
           <div className={styles.userInfo}>
             <label>간병 요일</label>
-            <span>detail 가져와야 됨...</span>
+            <span>
+              {normalizeWeekDays(data.weekdays)
+                .map((e) => weekdayDic[e])
+                .join(', ')}
+            </span>
           </div>
-          <div className={styles.userInfo}>
-            <label>출퇴근시간</label>
-            <span>detail 가져와야 됨...</span>
-          </div>
-          <div className={styles.userInfo}>
-            <label>시급</label>
-            <span>detail 가져와야 됨...</span>
-          </div>
+
           <div className={styles.userInfo}>
             <label>총 결제액</label>
-            <span>detail 가져와야 됨...</span>
+            <span>
+              {(
+                detailData?.wage *
+                calTimeDiff(data.start_time, data.end_time) *
+                countWeekdays(data.start_date, data.end_date, data.weekdays)
+              ).toLocaleString()}
+              원
+            </span>
           </div>
         </div>
       </div>
       <div className={styles.search_button_wrapper}>
-        <button type='button' id={data.id} onClick={(e) => handleReservationDetailButton(e)}>
-          예약 상세정보
+        <button type='button' id={data.id} onClick={() => handleReservationDetailButton(detailData)}>
+          간병 상세정보
         </button>
-        <button type='button' onClick={(e) => handlePaymentButton(e)}>
+        <button type='button' onClick={() => handlePaymentButton(detailData)}>
           결제하기
         </button>
       </div>
