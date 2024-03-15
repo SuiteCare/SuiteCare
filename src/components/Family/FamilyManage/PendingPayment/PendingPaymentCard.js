7c5e35c09092cb7a8391c1970a10690904d44754
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2024-03-16 00:28:03 +0900

    [style] 결제목록 디자인 수정

--- src/components/Family/FamilyManage/PendingPayment/PendingPaymentCard.js
+++ src/components/Family/FamilyManage/PendingPayment/PendingPaymentCard.js
@@ -37,66 +37,75 @@ const PaymentCard = ({ data, handleReservationDetailButton, handlePaymentButton 
   return (
     <div className={styles.card}>
       {isDetailDataLoading && <LocalLoading />}
-      {data?.profile_picture_filename || <Image src={defaultProfile} alt='profile_picture' />}
-      <div className={styles.contents}>
-        <div className={styles.userName}>
-          <span>간병 메이트 </span>
-          <label>
-            {data.mate_name} ({data.mate_resume_id})
-          </label>
-          <hr />
-        </div>
-        <div className={styles.userInfo_wrapper}>
-          <div className={styles.userInfo}>
-            <label>간병 위치</label>
-            <span>{detailData?.location}</span>{' '}
-            <span>
-              {detailData?.road_address} {detailData?.address_detail}
-            </span>
+      <div className={styles.top}>
+        <span className={`${detailData?.location === '병원' ? styles.hospital : styles.home}`}>
+          {detailData?.location}
+        </span>
+        <span>
+          {detailData?.road_address} {detailData?.address_detail}
+        </span>
+      </div>
+      <section>
+        {data?.profile_picture_filename || (
+          <Image src={defaultProfile} alt='profile_picture' width={300} height={200} />
+        )}
+        <div className={styles.contents}>
+          <div className={styles.userName}>
+            <span>간병 메이트 </span>
+            <label>
+              {data.mate_name} ({data.mate_resume_id})
+            </label>
           </div>
-          <div className={styles.userInfo}>
-            <label>담당 환자</label>
-            <span>
-              {detailData?.patient_name} ({detailData?.patient_diagnosis_name})
-            </span>
-          </div>
-          <div className={styles.userInfo}>
-            <label>간병 기간</label>
-            <span>
-              {data.start_date} ~ {data.end_date} (총 {countWeekdays(data.start_date, data.end_date, data.weekdays)}
-              일)
-            </span>
-          </div>
-          <div className={styles.userInfo}>
-            <label>간병 요일</label>
-            <span>
-              {normalizeWeekDays(data.weekdays)
-                .map((e) => weekdayDic[e])
-                .join(', ')}
-            </span>
-          </div>
-
-          <div className={styles.userInfo}>
-            <label>총 결제액</label>
-            <span>
-              {(
-                detailData?.wage *
-                calTimeDiff(data.start_time, data.end_time) *
-                countWeekdays(data.start_date, data.end_date, data.weekdays)
-              ).toLocaleString()}
-              원
-            </span>
+          <div className={styles.userInfo_wrapper}>
+            <div className={styles.userInfo}>
+              <label>담당 환자</label>
+              <span>
+                {detailData?.patient_name} ({detailData?.patient_diagnosis_name})
+              </span>
+            </div>
+            <div className={styles.userInfo}>
+              <label>간병 기간</label>
+              <span>
+                {data.start_date} ~ {data.end_date} (총 {countWeekdays(data.start_date, data.end_date, data.weekdays)}
+                일)
+              </span>
+            </div>
+            <div className={styles.userInfo}>
+              <label>간병 요일</label>
+              <span>
+                {normalizeWeekDays(data.weekdays)
+                  .map((e) => weekdayDic[e])
+                  .join(', ')}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.search_button_wrapper}>
-        <button type='button' id={data.id} onClick={() => handleReservationDetailButton(detailData)}>
-          간병 상세정보
-        </button>
-        <button type='button' onClick={() => handlePaymentButton(detailData)}>
-          결제하기
-        </button>
-      </div>
+        <div className={styles.search_button_wrapper}>
+          <button type='button' id={data.id} onClick={() => handleReservationDetailButton(detailData)}>
+            간병 상세정보
+          </button>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <span />
+        <div>
+          <label>총 결제액</label>
+          <span>
+            {(
+              detailData?.wage *
+              calTimeDiff(data.start_time, data.end_time) *
+              countWeekdays(data.start_date, data.end_date, data.weekdays)
+            ).toLocaleString()}
+            원
+          </span>
+        </div>
+        <div className={styles.search_button_wrapper}>
+          <button type='button' onClick={() => handlePaymentButton(detailData)}>
+            결제하기
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
