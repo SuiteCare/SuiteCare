1f604be0c5658cbfcf270d462ba0077dbd2236a0
Lee Jae Sun <ze_s96@naver.com>
2024-04-15 10:53:22 +0900

    [feat] 결제 성공시 서버에 성공 여부 넘겨주기

--- src/pages/family/payment/kakaopay/success.jsx
+++ src/pages/family/payment/kakaopay/success.jsx
@@ -1,10 +1,36 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSearchParams} from "next/navigation";
+import axiosInstance from "@/services/axiosInstance";
 
 const KakaopaySuccess = () => {
+  const params = useSearchParams();
+
+  const pay = async (params) => {
+    const body = {
+      pg_token: params.get('pg_token'),
+      reservation_id: params.get("reservation_id"),
+    };
+
+    if(body.pg_token) {
+      try {
+        const response = await axiosInstance.post('/api/v1/payment/success', body);
+        return response;
+      } catch (error) {
+        throw error;
+      }
+    }
+  };
+
   useEffect(() => {
-    alert('결제가 완료되었습니다.');
-    window.close();
-  }, []);
+    pay(params).then(r => {
+      if(r.data.code === 200) {
+        alert("결제 완료!!")
+        window.close();
+      }
+    }).catch(e => console.error(e));
+  }, [params]);
+
+
   return <div />;
 };
 
